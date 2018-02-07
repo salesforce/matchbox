149c5771803abe2eecbd994957c9c5bf0f5ef37d
James Bradbury <jekbradbury@gmail.com>
2018-02-06 17:54:20 -0800

    more transformer tests

--- examples/test_transformer.py
+++ examples/test_transformer.py
@@ -1,6 +1,17 @@
-from matchbox.test_utils import mb_test
+import argparse
+import random
 
-from transformer import LayerNorm, FeedForward, ResidualBlock, Attention, MultiHead
+import torch
+from torch import nn
+from torch.autograd import Variable
+
+import matchbox
+from matchbox import MaskedBatch
+from matchbox import functional as F
+from matchbox.test_utils import mb_test, mb_rand, mb_assert
+from matchbox.data import MaskedBatchField
+
+from transformer import *
 
 def test_LayerNorm():
     mb_test(LayerNorm(2),
@@ -27,3 +38,31 @@ def test_MultiHead():
             (4, (True, 3), (False, 6)), (4, (True, 3), (False, 6)), 1)
     mb_test(MultiHead(Attention(6, 0, True), 6, 6, 3),
             (4, (True, 3), (False, 6)), 0, 0)
+
+def test_EncoderLayer():
+    args = argparse.Namespace()
+    args.__dict__.update(d_model=6, d_hidden=6, n_heads=3, drop_ratio=0)
+    mb_test(EncoderLayer(args),
+            (4, (True, 3), (False, 6)))
+
+def test_DecoderLayer():
+    args = argparse.Namespace()
+    args.__dict__.update(d_model=6, d_hidden=6, n_heads=3, drop_ratio=0)
+    mb_test(DecoderLayer(args),
+            (4, (True, 3), (False, 6)), (4, (True, 3), (False, 6)))
+
+def test_posenc():
+    mb_test(lambda x: x + positional_encodings_like(x),
+            (4, (True, 3), (False, 6)))
+
+def test_Encoder():
+    args = argparse.Namespace()
+    args.__dict__.update(d_model=6, d_hidden=6, n_heads=3, drop_ratio=0,
+                         n_layers=2)
+    field = MaskedBatchField()
+    field.out = nn.Linear(args.d_model, 5)
+    xs = [Variable(torch.rand(1, random.randint(1, 3)).mul_(5).long())
+          for i in range(4)]
+    xb = MaskedBatch.fromlist(xs, (True,))
+    mb_assert(Encoder(field, args),
+             (xs,), (xb,), 4)
