5f5764b8a88f50ffb6690f2517505b633a38c9d9
James Bradbury <jekbradbury@gmail.com>
2018-02-08 10:25:38 -0800

    more rnn tests that work

--- test/test_rnns.py
+++ test/test_rnns.py
@@ -25,5 +25,18 @@ def test_rnn():
         return inner
     mb_test(SimpleRNN(nn.RNNCell(2, 2)),
             (4, (True, 3), (False, 2)), (4, (False, 2)))
-    # mb_test(SimpleRNN(nn.RNNCell(2, 2)),
-    #         (4, (True, 3), (False, 2)), (4, (False, 2)))
+
+class RNNClass(nn.Module):
+    def __init__(self, cell):
+        super().__init__()
+        self.cell = cell
+    @wrap
+    def forward(self, x, h0=None):
+        h = x.new(1, x.size(-1)).zero_() if h0 is None else h0
+        for xt in x.unbind(1):
+            h = self.cell(xt, h)
+        return h
+
+def test_rnn_class():
+    mb_test(RNNClass(nn.RNNCell(2, 2)),
+            (4, (True, 3), (False, 2)))
