2ebfa8d2c671da9f9667fa7656c3c4c406bad306
James Bradbury <jekbradbury@gmail.com>
2018-02-08 14:21:30 -0800

    add LSTM test

--- test/test_rnns.py
+++ test/test_rnns.py
@@ -40,3 +40,21 @@ class RNNClass(nn.Module):
 def test_rnn_class():
     mb_test(RNNClass(nn.RNNCell(2, 2)),
             (4, (True, 3), (False, 2)))
+
+class LSTMClass(nn.Module):
+    def __init__(self, in_size, out_size):
+        super().__init__()
+        self.cell = nn.LSTMCell(in_size, out_size)
+    @wrap
+    def forward(self, x, h0=None, c0=None):
+        h = x.new(1, x.size(-1)).zero_() if h0 is None else h0
+        c = x.new(1, x.size(-1)).zero_() if c0 is None else c0
+        for xt in x.unbind(1):
+            state = self.cell(xt, (h, c))
+            h = state[0]
+            c = state[1]
+        return h
+
+def test_lstm_class():
+    mb_test(LSTMClass(2, 2),
+            (4, (True, 3), (False, 2)))
