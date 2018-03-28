ffdf0f4681d1dea45511a3248118f343396b9aad
James Bradbury <jekbradbury@gmail.com>
2018-03-28 00:34:39 -0700

    license headers

--- examples/transformer.py
+++ examples/transformer.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2018, salesforce.com, inc.
+# All rights reserved.
+# Licensed under the BSD 3-Clause license.
+# For full license text, see the LICENSE file in the repo root
+# or https://opensource.org/licenses/BSD-3-Clause
+
 import torch
 from torch import nn
 from torchtext import data, datasets
--- matchbox/recompile/__init__.py
+++ matchbox/recompile/__init__.py
@@ -1,2 +1,8 @@
+# Copyright (c) 2018, salesforce.com, inc.
+# All rights reserved.
+# Licensed under the BSD 3-Clause license.
+# For full license text, see the LICENSE file in the repo root
+# or https://opensource.org/licenses/BSD-3-Clause
+
 from .compile_function import compile_function
 from .code_to_ast import code_to_ast
