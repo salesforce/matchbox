81d19553fdd091653d5e7e13c8214cbaa466a2c4
cclauss <cclauss@bluewin.ch>
2018-03-29 15:20:13 +0200

    import six for line 50 of data.py
    Prior to this change, __six__ is an undefined name in this context.

--- matchbox/data.py
+++ matchbox/data.py
@@ -4,6 +4,7 @@
 # For full license text, see the LICENSE file in the repo root
 # or https://opensource.org/licenses/BSD-3-Clause
 
+import six
 import torch
 from torch.autograd import Variable
 from torchtext import data
