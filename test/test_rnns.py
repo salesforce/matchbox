860b5b9c7ecd065066fd880eb4a70d8567fd2e64
James Bradbury <jekbradbury@gmail.com>
2018-05-14 20:35:34 -0700

    move stuff into test functions so compilation happens at test runtime
    (also updates some tests to use batch_zeros and splits some multiple
    assignments)

--- test/test_rnns.py
+++ test/test_rnns.py
@@ -17,14 +17,13 @@ import random
 def test_rnn_cell():
     mb_test(nn.RNNCell(2, 2), (4, (False, 2)), (4, (False, 2)))
 
-@batch
-def simple_rnn(x, h0, cell):
-    h = h0
-    for xt in x.unbind(1):
-        h = cell(xt, h)
-    return h
-
 def test_rnn():
+    @batch
+    def simple_rnn(x, h0, cell):
+        h = h0
+        for xt in x.unbind(1):
+            h = cell(xt, h)
+        return h
     def SimpleRNN(cell):
         def inner(x, h0):
             return simple_rnn(x, h0, cell)
@@ -65,28 +64,27 @@ def test_lstm_class():
     mb_test(LSTMClass(2, 2),
             (4, (True, 3), (False, 2)))
 
-class BiLSTMClass(nn.Module):
-    def __init__(self, in_size, out_size):
-        super().__init__()
-        self.fcell = nn.LSTMCell(in_size, out_size)
-        self.rcell = nn.LSTMCell(in_size, out_size)
-    @batch
-    def forward(self, x, h0=None, c0=None):
-        hf = x.new(x.size(0), x.size(-1)).zero_() if h0 is None else h0
-        cf = x.new(x.size(0), x.size(-1)).zero_() if c0 is None else c0
-        for xt in x.unbind(1):
-            state = self.fcell(xt, (hf, cf))
-            hf = state[0]
-            cf = state[1]
-        hr = x.new(x.size(0), x.size(-1)).zero_() if h0 is None else h0
-        cr = x.new(x.size(0), x.size(-1)).zero_() if c0 is None else c0
-        for xt in reversed(x.unbind(1)):
-            state = self.rcell(xt, (hr, cr))
-            hr = state[0]
-            cr = state[1]
-        return hf, hr
-
 def test_bilstm_class():
+    class BiLSTMClass(nn.Module):
+        def __init__(self, in_size, out_size):
+            super().__init__()
+            self.fcell = nn.LSTMCell(in_size, out_size)
+            self.rcell = nn.LSTMCell(in_size, out_size)
+        @batch
+        def forward(self, x, h0=None, c0=None):
+            hf = x.batch_zeros(x.size(-1)) if h0 is None else h0
+            cf = x.batch_zeros(x.size(-1)) if c0 is None else c0
+            for xt in x.unbind(1):
+                state = self.fcell(xt, (hf, cf))
+                hf = state[0]
+                cf = state[1]
+            hr = x.batch_zeros(x.size(-1)) if h0 is None else h0
+            cr = x.batch_zeros(x.size(-1)) if c0 is None else c0
+            for xt in reversed(x.unbind(1)):
+                state = self.rcell(xt, (hr, cr))
+                hr = state[0]
+                cr = state[1]
+            return hf, hr
     mb_test(BiLSTMClass(2, 2),
             (4, (True, 3), (False, 2)))
 
@@ -110,27 +108,28 @@ def test_accum_rnn_class():
     mb_test(AccumRNNClass(nn.RNNCell(2, 2), True),
             (4, (True, 3), (False, 2)))
 
-class AccumBiRNNClass(nn.Module):
-    def __init__(self, size):
-        super().__init__()
-        self.fwd = nn.RNNCell(size, size)
-        self.bwd = nn.RNNCell(size, size)
-    @batch
-    def forward(self, x):
-        h = h0 = x.batch_zeros(x.size(-1))
-        fwd, bwd = [], []
-        for xt in x.unbind(1):
-            h = self.fwd(xt, h)
-            fwd.append(h)
-        fwd = F.stack(fwd, 1)
-        h = h0
-        for xt in reversed(x.unbind(1)):
-            h = self.bwd(xt, h)
-            bwd.append(h)
-        bwd = F.stack(reversed(bwd), 1)
-        return F.cat((fwd, bwd), 2)
-
 def test_accum_birnn_class():
+    class AccumBiRNNClass(nn.Module):
+        def __init__(self, size):
+            super().__init__()
+            self.fwd = nn.RNNCell(size, size)
+            self.bwd = nn.RNNCell(size, size)
+        @batch
+        def forward(self, x):
+            h0 = x.batch_zeros(x.size(-1))
+            h = h0
+            fwd = []
+            bwd = []
+            for xt in x.unbind(1):
+                h = self.fwd(xt, h)
+                fwd.append(h)
+            fwd = F.stack(fwd, 1)
+            h = h0
+            for xt in reversed(x.unbind(1)):
+                h = self.bwd(xt, h)
+                bwd.append(h)
+            bwd = F.stack(reversed(bwd), 1)
+            return F.cat((fwd, bwd), 2)
     mb_test(AccumBiRNNClass(1),
             (4, (True, 3), (False, 1)))
 
