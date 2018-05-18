0c0ce205a1e987c3920e07d2d73300924508f86c
James Bradbury <jekbradbury@gmail.com>
2018-05-17 18:43:22 -0700

    add __setitem__ and advanced indexing for __getitem__
    I'm pretty confident there's a logic bug somewhere in this commit.

--- matchbox/functional/indexing.py
+++ matchbox/functional/indexing.py
@@ -6,32 +6,91 @@
 
 import torch
 
-from matchbox import MaskedBatch
+from matchbox import MaskedBatch, BD
 from matchbox.compat import MAYBE_VARIABLE, TENSOR_TYPE
 
-def getitem(batch, index):
-    if not isinstance(index, tuple) or index[0] != slice(None):
-        raise ValueError("first index must be :")
-    if None in index:
-        raise NotImplementedError("cannot index with None")
-    data = batch.data[index]
+def _check_index(batch, index):
+    if not isinstance(index, tuple) or index[0] is not BD:
+        return index, None
+    # TODO may be able to use _check_advanced_indexing
+    # and/or _preprocess_adv_index_seq
+    if any(map(lambda i: isinstance(i, (MaskedBatch, TENSOR_TYPE, list, tuple)),
+               index[1:])):
+        # advanced indexing case
+        return (torch.arange(batch.size(0)).long(),) + index[1:], True
+    if any(map(lambda i: i is None, index)):
+        return (slice(None),) + index[1:], None
+    # basic slicing case
+    return (slice(None),) + index[1:], False
+
+def _fix_negative_mask_slices(batch, index):
     index = list(index)
     for i, (ind, b) in enumerate(zip(index[1:], batch.dims)):
         if b:
             if isinstance(ind, int) and ind < 0:
                 raise NotImplementedError("cannot index dynamic dim with "
                                           "negative integer")
-            if isinstance(ind, slice) and ind.stop is not None and ind.stop < 0:
+            if (isinstance(ind, slice) and ind.stop is not None
+                    and ind.stop < 0):
                 if ind.step is not None or ind.start is not None:
-                    raise NotImplementedError("cannot index dynamic dim with "
-                                              "complex slice")
+                    raise NotImplementedError("cannot index dynamic dim "
+                                              "with complex slice")
                 index[i + 1] = slice(-ind.stop, None)
-    index = tuple(index)
-    mask = batch.mask[tuple(i if b else 0 if isinstance(i, int) else slice(None)
-                       for i, b in zip(index, (True,) + batch.dims))]
-    dims = tuple(b for i, b in zip(index[1:] + (slice(None),) * len(batch.dims),
-                                   batch.dims)
-                 if not isinstance(i, int)) # could be faster
-    return MaskedBatch(data, mask, dims)
+    return tuple(index)
 
-MaskedBatch.__getitem__ = getitem
+def _wrap_getitem(original):
+    def getitem(batch, index):
+        index, advanced = _check_index(batch, index)
+        if not isinstance(batch, MaskedBatch):
+            return original(batch, index)
+        if advanced is None:
+            raise NotImplementedError("unimplemented indexing pattern")
+        indexdata = tuple(i.data if isinstance(i, MaskedBatch)
+                          else i for i in index)
+        indexmask = tuple(i.mask if isinstance(i, MaskedBatch)
+                          else slice(None) for i in index)
+        data = batch.data[indexdata]
+        indexdata = _fix_negative_mask_slices(batch, indexdata)
+        mask = batch.mask.same_zeros()
+        mask[indexmask] = 1
+        mask = batch.mask * mask
+        mask = mask[tuple(i if b else 0 if isinstance(i, int)
+                          else slice(None) for i, b in zip(
+                            indexdata, (True,) + batch.dims))]
+        dims = tuple(b for i, b in zip(
+            index[1:] + (slice(None),) * len(batch.dims), batch.dims)
+                     if isinstance(i, slice)) # could be faster
+        return MaskedBatch(data, mask, dims)
+    return getitem
+
+MaskedBatch.__getitem__ = TENSOR_TYPE.__getitem__ = _wrap_getitem(
+    TENSOR_TYPE.__getitem__)
+
+def _wrap_setitem(original):
+    def setitem(batch, index, value):
+        index, advanced = _check_index(batch, index)
+        if not isinstance(batch, MaskedBatch):
+            original(batch, index, value)
+            return
+        if advanced is None:
+            raise NotImplementedError("unimplemented indexing pattern")
+        if not isinstance(value, MaskedBatch):
+            batch.data[index] = value
+            return batch # TODO should MaskedBatch be mutable?
+            # maybe a rule is to allow mutating _only_ the data
+        data = batch.data.clone() # TODO make in-place
+        indexdata = tuple(i.data if isinstance(i, MaskedBatch)
+                          else i for i in index)
+        indexmask = tuple(i.mask if isinstance(i, MaskedBatch)
+                          else slice(None) for i in index)
+        data[indexdata] = value.data
+        batchmask = batch.mask.clone()
+        mask = value.mask.same_zeros()
+        mask[indexmask] = 1
+        batchmask[indexdata] = value.mask * mask
+        data = torch.where(batchmask.byte(), data, batch.data)
+        return MaskedBatch(data, batch.mask, batch.dims)
+    return setitem
+
+MaskedBatch.__setitem__ = TENSOR_TYPE.__setitem__ = _wrap_setitem(
+    TENSOR_TYPE.__setitem__)
